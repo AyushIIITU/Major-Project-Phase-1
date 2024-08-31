327dba327a1b91cdcf564cfff600b74c6aec683b
AyushIIITU <ayush01122004@gamil.com>
2024-08-31 10:55:20 +0530

    Add NavBAr

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,15 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavBarReact from './Components/NavBar/NavBar'
+// import { NavbarReact } from './Components/NavBar/NavBar'
 
 function App() {
 
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  return (<>
+    <NavBarReact/>
+    <Outlet/>
+  </>
   )
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Home from './Components/Home/Home.jsx';
+const router=createBrowserRouter(
+  [
+    {
+      path: "/",
+        element: <App />,
+      children:[
+        {
+      path: "",
+      element: <Home/>,
+        }
+      ]
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    
+    }
+  ]
 )
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <ThemeProvider/> */}
+    <RouterProvider router={router} />
+    <Toaster position="top-center" reverseOrder={false} />
+  </React.StrictMode>
+);
